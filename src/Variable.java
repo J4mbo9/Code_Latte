67fa2687e956015716344c6cf7bc9e3723bbe939
J4mbo9 <chkfrdlek@naver.com>
2022-07-31 22:23:04 +0900

    2022.07.31 코드라떼 변수

---
+++ src/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+        public static void main(String[] args) {
+            int num = 100;
+            num = 200;
+
+            System.out.println(num);
+        }
+    }
