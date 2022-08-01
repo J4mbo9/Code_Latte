e9ccfb27baaf4a045a349bb5d31ce6b65477ffca
J4mbo9 <chkfrdlek@naver.com>
2022-08-02 00:05:26 +0900

    2022.08.01 코드라떼 메소드 수정

--- src/Method.java
+++ src/Method.java
@@ -1,17 +1,28 @@
 public class Method {
 
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("춘식");
+        dog.eat("개밥");
+        dog.running();
+    }
+}
     class Dog {
         String name;
+
+
+        Dog(String name) {
+            this.name = name;
+        }
+
+        void eat(String food) {
+            System.out.println(food + "을 먹는다");
+
+        }
+
+        static void running() { //정적메소드
+            System.out.println("달린다");
+        }
     }
 
-    void eat(String food){
-        System.out.println(food + "을 먹는다");
 
-    }
-    static void running(){
-        System.out.println("달린다");
-    }
-
-    Dog dog = new Dog();
-    dog.setName
-}
