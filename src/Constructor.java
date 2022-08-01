314dcf1ec5dac35804265ac7fad34875083c55dd
J4mbo9 <chkfrdlek@naver.com>
2022-08-01 23:45:42 +0900

    2022.08.01 코드라떼 생성자 오버로딩 추가

--- src/Constructor.java
+++ src/Constructor.java
@@ -1,17 +1,31 @@
 public class Constructor {
 
-    class Pserson {
+    class Person {
         String name;
         int age;
         String sex;
 
 
-        Pserson(String name, int age, String sex) { //생성자를 통해 인스턴스 생성 가능
+        Person(String name) {
+            this.name = name;
+
+        }
+
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        Person(String name, int age, String sex) { //생성자를 통해 인스턴스 생성 가능
             this.name = name;
             //this.name 클래스의 멤버변수, name 생성자의 매개변수를 나타냄
             this.age = age;
             this.sex = sex;
         }
+
     }
-        Pserson pserson = new Pserson("박찬현",30,"남자");
+        Person person = new Person("박찬현");
+        Person person2 = new Person("박찬현", 30);
+        Person person1 = new Person("박찬현", 30, "남자");
 }
