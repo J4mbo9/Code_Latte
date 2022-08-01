45711b6ec611b466489b37a3bdff23d8639d9279
J4mbo9 <chkfrdlek@naver.com>
2022-08-01 23:59:17 +0900

    2022.08.01 코드라떼 메소드

---
+++ src/Method.java
@@ -0,0 +1,17 @@
+public class Method {
+
+    class Dog {
+        String name;
+    }
+
+    void eat(String food){
+        System.out.println(food + "을 먹는다");
+
+    }
+    static void running(){
+        System.out.println("달린다");
+    }
+
+    Dog dog = new Dog();
+    dog.setName
+}
